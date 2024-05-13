1b17e35b0c3a02f611a53f6b8a6327b47388afc2
Adam Halnon <halnon_01@hotmail.com>
2024-05-13 17:22:32 +0100

    minor changes to make code more readable

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-export const Tile = ({name, description}) => {
+export const Tile = ({ name, description }) => {
   return (
     <div className="tile-container">
-      <p className="tile-title">{name}</p>
+      <p className="tile-title tile">
+        {name}
+      </p>
       {Object.values(description).map((value, index) => {
-        <p key={index} className="tile">{value}</p>
+        <p key={index} className="tile">
+          {value}
+        </p>
       })}
     </div>
   );
