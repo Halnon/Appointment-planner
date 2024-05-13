abefdaee04acb49c7d6f3a312a3d05223a13272e
Adam Halnon <halnon_01@hotmail.com>
2024-05-13 18:29:00 +0100

    add comparison op to fix type error bug

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -4,12 +4,9 @@ import { Tile } from "../tile/Tile";
 export const TileList = ({ tiles }) => {
   return (
     <div>
-      {tiles.map((tile, index) => {
+      {tiles && tiles.map((tile, index) => {
         const {name, ...description } = tile;
-        return <Tile 
-                  key={index} 
-                  name={name} 
-                  description={description}/>
+        return <Tile key={index} name={name} description={description}/>
     })}
     </div>
   );
