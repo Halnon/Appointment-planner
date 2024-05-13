86a7ef5074fc7991487148d74e3745e3a44be0bd
Adam Halnon <halnon_01@hotmail.com>
2024-05-13 16:15:03 +0100

    add input labels and props

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -12,7 +12,41 @@ export const ContactForm = ({
   return (
     <>
       <form onSubmit={handleSubmit}>
-
+        <label>
+          <input
+            type='text' 
+            name='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeHolder='Contact Name'
+            aria-label='Contact Name'
+          />
+        </label>
+        <hr/>
+        <label>
+          <input
+            type='tel'
+            name='phone'
+            pattern="^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            placeHolder='Contact Phone'
+            aria-label='Contact Phone'
+          />
+        </label>
+        <hr/>
+        <label>
+          <input
+            type='email' 
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeHolder='Contact Email'
+            aria-label='Contact Email'
+          />
+        </label>
+        <hr/>
+        <input type='submit' value='Add contact' aria-label='Add contact'/>
       </form>
     </>
   );
